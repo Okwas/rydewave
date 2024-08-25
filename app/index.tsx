4036f22b0859e631c32089aacf8984100e92bd97
demon <naimadokwas@gmail.com>
2024-08-25 13:19:09 +0200

    welcome onboarding

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
-const Page = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+const Home = () => {
+  return <Redirect href="/(auth)/welcome" />;
 };
 
-export default Page;
+export default Home;
